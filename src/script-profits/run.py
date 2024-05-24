f4b599e13181bdf1d9a0e9e8d30133df0269db25
r-spiewak <63987228+r-spiewak@users.noreply.github.com>
2024-05-24 10:33:31 -0400

    Add cli params

--- src/script-profits/run.py
+++ src/script-profits/run.py
@@ -1,16 +1,25 @@
 import typer
+from typing import Optional
+from typing_extensions import Annotated
 
 app = typer.Typer()
 
 @app.command()
-def main() -> int:
+def main(
+    data: Annotated[str, typer.Option(...,"--data","-d")],
+    percentage: Annotated[Optional[int], typer.Option("--percentage","-p")] = 10,
+) -> int:
     """Main function."""
 
     # Do stuff here.
     print("It worked!")
+    print(f"data: {data}")
+    print(f"percentage: {percentage}")
 
     return 0
 
+def run() -> None:
+    app()
 
 if __name__ == "__main__":
-    main()
+    app()
