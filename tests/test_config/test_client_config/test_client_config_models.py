57cccc8a83d29249bd423c002cef5f5474e167d8
r-spiewak <63987228+r-spiewak@users.noreply.github.com>
2025-06-20 18:08:02 -0400

    Add tests to complete client_config_models.py coverage

---
+++ tests/test_config/test_client_config/test_client_config_models.py
@@ -0,0 +1,18 @@
+"""This file has tests for client_config_models.py."""
+
+import pytest
+
+from time_tracker.config import Client
+
+
+def test_client_invalid_rate():
+    """Test Client with an invalid rate."""
+    with pytest.raises(ValueError):
+        Client(
+            name="bob",
+            address="123 Fourth Ave\nFive City, Sixth State, 00000",
+            email="bob@example.com",
+            phone="987-654-3210",
+            rate=-10,
+            filename="file.csv",
+        )
