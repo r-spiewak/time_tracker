944fa76b77fe50c05228a120816288194f2514f1
r-spiewak <63987228+r-spiewak@users.noreply.github.com>
2025-06-20 17:54:49 -0400

    Add tests to complete tracker coverage

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -2,11 +2,14 @@
 
 import csv
 import os
+import tempfile
 from datetime import datetime, timedelta
 from pathlib import Path
+from subprocess import CalledProcessError
 
 import pytest
 
+from time_tracker import TimeTracker
 from time_tracker.constants import HEADERS, ColumnHeaders
 
 INVALID_DATE_FORMAT = "Invalid date format"
@@ -247,3 +250,181 @@ def test_no_entries_no_crash(temp_tracker, capsys):
     tracker = temp_tracker
     tracker.report()
     assert NO_ENTRIES in capsys.readouterr().out
+
+
+def test_client_not_in_client_config(capsys):
+    """Tests when client isn't in client_config."""
+    temp_dir = tempfile.mkdtemp()
+    client = "client-1"
+    msg = f"Client {client} not in client config."
+    filename = f"{client}.csv"
+    logger_filename = (
+        f"{temp_dir}/"
+        f"logs/{datetime.now().strftime('%Y-%m-%d_%H.%M.%S.%f_%z')}"
+        f"_{type(TimeTracker).__name__}_.log"
+    )
+    tracker = TimeTracker(
+        directory=temp_dir, client=client, logger_filename=logger_filename
+    )
+    assert msg in capsys.readouterr().out
+    assert tracker.filepath == Path(temp_dir) / filename
+
+
+def test_generate_invoice(
+    tmp_path, mocker, mock_tracker_logger
+):  # pylint: disable=unused-argument,too-many-locals
+    """Test the generate_invoice method."""
+    two = 2
+    # Setup paths:
+    tex_template = tmp_path / "invoice_template.tex"
+    tex_template.write_text("Invoice ((( total )))")
+
+    mocker.patch("time_tracker.tracker.DEFAULT_INVOICE_DIR", tmp_path)
+    mocker.patch("time_tracker.tracker.DEFAULT_INVOICE_TEMPLATE", tex_template)
+    mocker.patch("time_tracker.tracker.SAMPLE_INVOICE_TEMPLATE", tex_template)
+
+    mocker.patch(
+        "time_tracker.tracker.get_next_invoice_number", return_value=999
+    )
+    mocker.patch(
+        "time_tracker.tracker.prepare_logo_for_latex", return_value="logo.pdf"
+    )
+
+    # Tracker instance with mocks:
+    tracker = TimeTracker()
+    tracker.client = "test_client"
+    tracker.generate_report = mocker.Mock(
+        return_value=(
+            {"dev": 3600},  # 1 hour
+            [datetime(2023, 1, 1), datetime(2023, 1, 2)],
+        )
+    )
+    tracker.client_config = mocker.Mock()
+    tracker.client_config.clients = {
+        "test_client": mocker.Mock(rate=150, name="Test Co.")
+    }
+    tracker.me = mocker.Mock(logo_path="logo.svg")
+
+    mock_logger = mocker.Mock()
+    tracker.logger = mock_logger
+
+    mock_run = mocker.patch("time_tracker.tracker.subprocess.run")
+
+    # Run invoice generator:
+    tracker.generate_invoice()
+
+    # Check output .tex written:
+    tex_out = list(tmp_path.glob("*.tex"))
+    assert len(tex_out) == two
+    assert tex_template in tex_out
+    assert tex_out[0].read_text().startswith("Invoice")
+
+    # Check subprocess and logger call:
+    mock_run.assert_called_once()
+    mock_logger.info.assert_called_once_with(
+        f"✅ Invoice created: {tmp_path / tex_out[0].with_suffix('.pdf').name}"
+    )
+
+    # Test when filename is given:
+    tex_filename = tmp_path / "invoice_filename.tex"
+    tracker.generate_invoice(invoice_filename=tex_filename)
+    # Check output .tex written:
+    tex_out = list(tmp_path.glob("*.tex"))
+    assert tex_filename in tex_out
+    assert tex_filename.read_text().startswith("Invoice")
+
+    # Test when subprocess gives an error:
+    retcode = 1
+    command = "madeup"
+    output = "output"
+    err = "err"
+    error = CalledProcessError(
+        returncode=retcode,
+        cmd=command,
+        output=output,
+        stderr=err,
+    )
+
+    def mock_error(*args, **kwargs):  # pylint: disable=unused-argument
+        """Mock error"""
+        raise error
+
+    mocker.patch("time_tracker.tracker.subprocess.run", mock_error)
+    tracker.generate_invoice(invoice_filename=tex_filename)
+    assert (
+        mocker.call("❌ Failed to compile LaTeX invoice: %s", error)
+        in mock_logger.warning.call_args_list
+    )
+    assert (
+        mocker.call("📄 STDOUT:\n%s", output)
+        in mock_logger.warning.call_args_list
+    )
+    assert (
+        mocker.call("🐞 STDERR:\n%s", err)
+        in mock_logger.warning.call_args_list
+    )
+
+
+def test_init_config(
+    tmp_path, mocker, mock_tracker_logger
+):  # pylint: disable=unused-argument
+    """Test the init_config method."""
+    empty_dict_str = "{}"
+    template = "TEMPLATE"
+    # Create dummy sample files:
+    sample_client = tmp_path / "sample_client.json"
+    sample_client.write_text(empty_dict_str)
+    sample_template = tmp_path / "sample_template.tex"
+    sample_template.write_text(template)
+    sample_me = tmp_path / "sample_me.json"
+    sample_me.write_text(empty_dict_str)
+    sample_invoice_state = tmp_path / "sample_invoice_state.json"
+    sample_invoice_state.write_text(empty_dict_str)
+
+    dest_client = tmp_path / "client.json"
+    dest_template = tmp_path / "template.tex"
+    dest_me = tmp_path / "me.json"
+    dest_state = tmp_path / "invoice_state.json"
+
+    # with mocker.patch.multiple(
+    #     "time_tracker.tracker",
+    #     SAMPLE_CLIENT_CONFIG_FILE=sample_client,
+    #     SAMPLE_INVOICE_TEMPLATE=sample_template,
+    #     SAMPLE_ME_CONFIG_FILE=sample_me,
+    #     SAMPLE_INVOICE_STATE_CONFIG_FILE=sample_invoice_state,
+    #     DEFAULT_CLIENT_CONFIG_FILE=dest_client,
+    #     DEFAULT_INVOICE_TEMPLATE=dest_template,
+    #     DEFAULT_ME_CONFIG_FILE=dest_me,
+    #     DEFAULT_INVOICE_STATE_CONFIG_FILE=dest_state,
+    # ):
+    mocker.patch(
+        "time_tracker.tracker.SAMPLE_CLIENT_CONFIG_FILE", sample_client
+    )
+    mocker.patch(
+        "time_tracker.tracker.SAMPLE_INVOICE_TEMPLATE", sample_template
+    )
+    mocker.patch("time_tracker.tracker.SAMPLE_ME_CONFIG_FILE", sample_me)
+    mocker.patch(
+        "time_tracker.tracker.SAMPLE_INVOICE_STATE_CONFIG_FILE",
+        sample_invoice_state,
+    )
+    mocker.patch(
+        "time_tracker.tracker.DEFAULT_CLIENT_CONFIG_FILE", dest_client
+    )
+    mocker.patch(
+        "time_tracker.tracker.DEFAULT_INVOICE_TEMPLATE", dest_template
+    )
+    mocker.patch("time_tracker.tracker.DEFAULT_ME_CONFIG_FILE", dest_me)
+    mocker.patch(
+        "time_tracker.tracker.DEFAULT_INVOICE_STATE_CONFIG_FILE", dest_state
+    )
+    from time_tracker.tracker import (  # pylint: disable=import-outside-toplevel,reimported,redefined-outer-name
+        TimeTracker,
+    )
+
+    TimeTracker.init_config()
+
+    assert dest_client.read_text() == empty_dict_str
+    assert dest_template.read_text() == template
+    assert dest_me.read_text() == empty_dict_str
+    assert dest_state.read_text() == empty_dict_str
